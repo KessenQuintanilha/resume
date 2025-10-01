dd7cfdda05dd1fedadb6760c963bcf8f18b8af5d
KessenQuintanilha <kessen.17@gmail.com>
2025-10-01 17:45:03 +0100

    ajuste tela de celular

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component , HostListener, OnInit} from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home',
@@ -9,11 +13,28 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
     MatSidenavModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatIconModule,
+    CommonModule,
+
   ],
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
 export class Home {
 
+   isMobile = false;
+
+  ngOnInit() {
+    this.checkScreen();
+  }
+
+ @HostListener('window:resize')
+onResize() {
+  this.checkScreen();
+}
+
+  checkScreen() {
+    this.isMobile = window.innerWidth < 768; // você pode ajustar o breakpoint
+  }
 }
